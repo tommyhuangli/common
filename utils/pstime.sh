0b8b51f6ee4deefe936dad5e4f4a8ea9ccef6e36
Charlie Huang <charlie@AppleDTK.attlocal.net>
2020-12-25 14:04:15 -0800

    check mc elapsed time

--- utils/pstime.sh
+++ utils/pstime.sh
@@ -1,1 +1,10 @@
 ps -ax -o etime,pid,command|grep java |awk -v date="$(date +"%Y-%m-%d %r")" '{print $1,$2,$4,date}'|head -1 >> ~/.pstime.log
+p=`ps -p $(cat $HOME/.mcpid) | wc -l`
+etime_file="$HOME/.mcetime_`date +%y%m%d`"
+if [ -f $etime_file ]; then
+    echo "file existis"
+    etime=$(( `cat $etime_file` + 1))
+else
+    etime=0
+fi
+eval "echo $etime > $etime_file"
