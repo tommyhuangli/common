45733fa54d5d48a827a44ffeced679493d9b4ec9
Tommy Huang <tommyhuangli@gmail.com>
2022-10-25 20:06:44 -0700

    fix payload

--- bin/godaddy_updatedns.sh
+++ bin/godaddy_updatedns.sh
@@ -23,7 +23,7 @@ secret="LknJy67dNQeR5r5voPfa4W"         # secret for godaddy developer API
  
 headers="Authorization: sso-key $key:$secret"
  
- echo $headers
+echo $headers
  
 result=$(curl -s -X GET -H "$headers" \
  "https://api.godaddy.com/v1/domains/$domain/records/$type/$name")
@@ -37,14 +37,14 @@ ret=$(curl -s GET "http://ipinfo.io/json")
 currentIp=$(echo $ret | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
  echo "currentIp:" $currentIp
  
- if [ $dnsIp != $currentIp ];
+if [ $dnsIp != $currentIp ];
  then
         echo "IP's are not equal, updating record"
         curl -X PUT "https://api.godaddy.com/v1/domains/$domain/records/$type/$name" \
 -H "accept: application/json" \
 -H "Content-Type: application/json" \
 -H "$headers" \
--d "[ { \"data\": \"$currentIp\", \"port\": $port, \"priority\": 0, \"protocol\": \"string\", \"service\": \"string\", \"ttl\"$
+-d "[ { \"data\": \"$currentIp\", \"port\": $port, \"priority\": 0, \"protocol\": \"string\", \"service\": \"string\", \"ttl\": $ttl }]"
 fi
  if [ $dnsIp = $currentIp ];
  then
